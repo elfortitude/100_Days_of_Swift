393365f38bb453eb06d4fbddc1740d599441203e
Elizaveta Usacheva <elizaaee@gmail.com>
2021-07-18 12:53:36 +0300

    Added Project1

---
+++ Project1/Project1/DetailViewController.swift
@@ -0,0 +1,50 @@
+//
+//  DetailViewController.swift
+//  Project1
+//
+//  Created by out-usacheva-ei on 15.07.2021.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+    
+    var selectedImage: String?
+    var countOfImages = 0
+    var numberOfImage = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        title = "Image \(numberOfImage) of \(countOfImages)"
+        navigationItem.largeTitleDisplayMode = .never
+        
+//      Show image
+        if let imageToLoad = selectedImage {
+            imageView.image = UIImage(named: imageToLoad)
+        }
+    }
+    
+//      Show and hide Navigation Controller bar
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Project1/README.md
@@ -0,0 +1,11 @@
+#  Project 1
+
+First iOS developing project in "100 days of Swift" challenge on website: www.hackingwithswift.com
+
+In this project we create mini photo album with photos from the National Oceanic and Atmospheric Administration (NOAA), which is a US government agency and thus produces public domain content that we can freely reuse. This is placed in file system of device. We met view controllers, storyboards and the FileManager class.
+
+## Initial page with list of images:
+![Screenshot](screen1.png)
+
+## Showing image
+![Screenshot](screen2.png)
