4b1f07fee26a0b33c00f4011e5bf3c284a2de8fc
elfortitude <elizabeth.usacheva@mail.ru>
2021-09-02 11:45:45 +0300

    added Project 18

---
+++ Project18/Project18/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Project18
+//
+//  Created by out-usacheva-ei on 01.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        for i in 1...100 {
+            print("Got number \(i)")
+        }
+    }
+
+}
+
---
+++ Project18/README.md
@@ -0,0 +1,6 @@
+#  Project 18
+
+The eighteenth iOS developing project in "100 days of Swift" challenge on website: www.hackingwithswift.com
+
+In this project we precticed and lerned about some methods for debugging, but we haven't these in code.
+
