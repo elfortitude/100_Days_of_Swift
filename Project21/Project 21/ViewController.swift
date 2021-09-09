5a33e693ecb5e2d2bc15c2448eda7b76035e592c
elfortitude <elizabeth.usacheva@mail.ru>
2021-09-09 20:22:41 +0300

    Added Project 21

---
+++ Project21/Project 21/ViewController.swift
@@ -0,0 +1,102 @@
+//
+//  ViewController.swift
+//  Project 21
+//
+//  Created by out-usacheva-ei on 08.09.2021.
+//
+
+import UIKit
+import UserNotifications
+
+class ViewController: UIViewController, UNUserNotificationCenterDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Register", style: .plain, target: self, action: #selector(registerLocal))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Shedule", style: .plain, target: self, action: #selector(sheduleLocal))
+    }
+
+    @objc func registerLocal() {
+        let center = UNUserNotificationCenter.current()
+        
+        center.requestAuthorization(options: [.alert, .badge, .sound]) { (granted, error) in
+            if granted {
+                print("Yay!")
+            } else {
+                print("D'oh")
+            }
+        }
+    }
+    
+    @objc func sheduleLocal(timeInterval: TimeInterval) {
+        registerCategories()
+        let center = UNUserNotificationCenter.current()
+        
+        let content = UNMutableNotificationContent()
+        content.title = "Late wake up call"
+        content.body = "The early bird catches the worm, but th second mouse gets the cheese."
+        content.categoryIdentifier = "alarm"
+        content.userInfo = ["customData": "fizzbuzz"]
+        content.sound = UNNotificationSound.default
+        
+        var dateComponents = DateComponents()
+        dateComponents.hour = 10
+        dateComponents.minute = 30
+//        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
+        
+        let trigger: UNTimeIntervalNotificationTrigger
+        if timeInterval == TimeInterval(86400) {
+            trigger = UNTimeIntervalNotificationTrigger(timeInterval: timeInterval, repeats: false)
+        } else {
+            trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+        }
+        
+        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+        center.add(request)
+    }
+    
+    func registerCategories() {
+        let center = UNUserNotificationCenter.current()
+        center.delegate = self
+        
+        let show = UNNotificationAction(identifier: "show", title: "Tell me more...", options: .foreground)
+        let remindLater = UNNotificationAction(identifier: "remindLater", title: "Remind me later", options: .foreground)
+        let category = UNNotificationCategory(identifier: "alarm", actions: [show, remindLater], intentIdentifiers: [])
+        
+        center.setNotificationCategories([category])
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+//        pull out the buried userInfo dictionary
+        let userInfo = response.notification.request.content.userInfo
+        
+        if let customData = userInfo["customData"] as? String {
+            print("Custom data received: \(customData)")
+            
+            switch response.actionIdentifier {
+            case UNNotificationDefaultActionIdentifier:
+//                the user swiped to unlock
+                let ac = UIAlertController(title: "Default identifier", message: nil, preferredStyle: .alert)
+                ac.addAction(UIAlertAction(title: "OK", style: .default))
+                present(ac, animated: true)
+            case "show":
+//                the user tapped our "show more info..." button
+                let ac = UIAlertController(title: "Show more identifier...", message: nil, preferredStyle: .alert)
+                ac.addAction(UIAlertAction(title: "OK", style: .default))
+                present(ac, animated: true)
+            case "remindLater":
+                let ac = UIAlertController(title: "Remind me later identifier...", message: "This notification will be displayed in 24 hours.", preferredStyle: .alert)
+                ac.addAction(UIAlertAction(title: "OK", style: .default))
+                present(ac, animated: true)
+                sheduleLocal(timeInterval: 86400)
+            default:
+                break
+            }
+        }
+        
+//        you must call the completion handler when you're done
+        completionHandler()
+    }
+}
+
---
+++ Project21/README.md
@@ -0,0 +1,28 @@
+#  Project 21
+
+The twenty-first iOS developing project in "100 days of Swift" challenge on website: www.hackingwithswift.com
+
+In this project we created very simple one-page application with one target: learning notifications. We practiced and learned about Notification Center, UNUserNotificationCenter, requesting permission for notifications, the different kinds of notification trigger, UNMutableNotificationContent, notifications requests, UNNotificationCategory and UNNotificationAction, also DateComponents.
+
+## Demonstration
+
+General screen of app. It has two buttons: the first (left) one is for the permission request, the second one (right) - for launching sheduled notifications.
+
+![Screenshot](screen1.png)
+
+Permission request for notification sending.
+
+![Screenshot](screen2.png)
+
+Notification on lock screen.
+
+![Screenshot](screen3.png)
+
+Notification actions by notification categories.
+
+![Screenshot](screen4.png)
+
+Alert Controller when app opens by tapping on "Remind me later" notification action. Also we launching new notifications per year.
+
+![Screenshot](screen5.png)
+
